af1133fef264d131703b40a7b148285da3eb7401
kenye <kenye@KENDRICK>
2022-03-14 18:42:45 -0400

    updated port settings

--- server.js
+++ server.js
@@ -60,7 +60,6 @@ io.on("connection", socket => {
 })
 
 
-
-const PORT = 3000 || process.env.PORT;
-
-server.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+server.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
